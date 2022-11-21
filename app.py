bd1c34b2754c68bce3b61231aed96198ebebc910
unknown <tgavkhare80@gmail.com>
2022-11-21 12:05:31 +0530

    trying to deploy

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def uploadImages(key):
 
 def aboutMe():
     
-    st.markdown(requests.get(os.getenv('ABOUT_ME','https://raw.githubusercontent.com/niranjangirhe/niranjangirhe/main/README.md')).text, unsafe_allow_html=True)
+    st.markdown(requests.get(os.getenv('ABOUT_ME','https://github.com/PrinceTG80')).text, unsafe_allow_html=True)
 
 # Panorama 
 
